5826a579e6b2b7829f715bf75d5036d9316de54f
mikolaura <98705336+mikolaura@users.noreply.github.com>
2022-02-14 16:20:12 +0200

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1334675.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 68492060, 68493649, 68493971 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/68491984/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/68491985/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/68491983/1/ammo.js', 'preload' : true},
+];
